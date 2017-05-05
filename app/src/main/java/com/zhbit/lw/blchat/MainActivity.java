27fef96e05aae9194ad0b1badc60a5b1263b7184
wjh876531738 <876531738@qq.com>
2017-05-05 22:36:09 +0800

    添加部分组件和菜单并添加strings文件

--- app/src/main/java/com/zhbit/lw/blchat/MainActivity.java
+++ app/src/main/java/com/zhbit/lw/blchat/MainActivity.java
@@ -1,13 +1,30 @@
 package com.zhbit.lw.blchat;
 
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initView();
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    private void initView() {
+        viewPager = (ViewPager) findViewById(R.id.viewpager);
+
     }
 }
