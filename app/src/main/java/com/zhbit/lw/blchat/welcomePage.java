de5f208cc34726b07d904b5ac5fd6448fcbeabb2
PowerDos <543541941@qq.com>
2017-05-07 16:56:38 +0800

    add welcome Page

---
+++ app/src/main/java/com/zhbit/lw/blchat/welcomePage.java
@@ -0,0 +1,68 @@
+package com.zhbit.lw.blchat;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.Interpolator;
+import android.view.animation.LinearInterpolator;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+
+/**
+ *
+ */
+public class welcomePage extends Activity {
+    private Handler handler = new Handler();
+    private ImageView imgLoading;
+    private RelativeLayout wel_page;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome_page);
+        imgLoading = (ImageView) findViewById(R.id.img_loading);
+        wel_page = (RelativeLayout) findViewById(R.id.activity_welcome_page);
+        //显示动画
+        showAnimation();
+
+        //使用handler的postDelayed实现延时跳转
+        handler.postDelayed(new Runnable() {
+            public void run() {
+                Intent intent = new Intent(welcomePage.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },2600);//2秒后跳转至应用主界面MainActivity
+    }
+
+    private void showAnimation(){
+        //页面渐变显示
+        //创建透明动画
+        AlphaAnimation alphaAnimation = new AlphaAnimation(0,1);
+        //创建
+        alphaAnimation.setDuration(1000);
+        //开始动画
+        wel_page.startAnimation(alphaAnimation);
+
+
+        //图片旋转
+        //创建旋转动画
+        RotateAnimation rotateAnimation = new RotateAnimation(
+                0, 760,
+                Animation.RELATIVE_TO_SELF, 0.5f,
+                Animation.RELATIVE_TO_SELF, 0.5f
+        );
+        //防止重复时卡顿
+        rotateAnimation.setInterpolator(new LinearInterpolator());
+        //设置重复次数
+        rotateAnimation.setRepeatCount(2);
+        //设置时间
+        rotateAnimation.setDuration(2600);
+        imgLoading.startAnimation(rotateAnimation);
+    }
+}
