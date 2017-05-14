800ff645461e0fb75048a44248e25eda7e6b964c
PowerDos <543541941@qq.com>
2017-05-14 19:03:23 +0800

    创建登陆界面

--- app/src/main/java/com/zhbit/lw/activity/LoginActivity.java
+++ app/src/main/java/com/zhbit/lw/activity/LoginActivity.java
@@ -2,14 +2,46 @@ package com.zhbit.lw.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.zhbit.lw.blchat.R;
 
 public class LoginActivity extends AppCompatActivity {
-
+    private EditText edtUserName;
+    private EditText edtPassword;
+    private Button btnSignIn;
+    private Button btnSignUp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        edtUserName = (EditText) findViewById(R.id.edt_username);
+        edtPassword = (EditText) findViewById(R.id.edt_password);
+        btnSignIn = (Button) findViewById(R.id.btn_sign_in);
+        btnSignUp = (Button) findViewById(R.id.btn_sign_up);
+        //初始化监听事件
+        InitListener();
+    }
+    private void InitListener(){
+        //登陆业务逻辑处理
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Login();
+            }
+        });
+        //注册业务逻辑处理
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+    //登陆按钮业务处理
+    private void Login(){
+
     }
 }
