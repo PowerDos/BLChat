eefd289f0f2766183ed392bafb77c8b8421cb292
wjh876531738 <876531738@qq.com>
2017-05-15 10:27:52 +0800

    alter table

--- app/src/main/java/com/zhbit/lw/model/dao/ChatTable.java
+++ app/src/main/java/com/zhbit/lw/model/dao/ChatTable.java
@@ -9,12 +9,8 @@ public class ChatTable {
     public static final String TABLE_NAME = "chat_msg";
 
     public static final String USER_ID = "user_id";
-    public static final String USER_NAME = "user_name";
-    public static final String USER_HEAD = "user_head";
-    public static final String TARGET_ID = "target_id";
-    public static final String TARGET_NAME = "target_name";
-    public static final String TARGET_HEAD = "target_head";
-    public static final String TARGET_EXPAND_RELATION = "target_expand_relation";
+    public static final String FRIEND_ID = "friend_id";
+    public static final String FRIEND_EXPAND_RELATION = "friend_expand_relation";
     public static final String CHAT_MSG_CONTENT = "chat_msg_content";
     public static final String CHAT_MSG_TIME = "chat_msg_time";
     public static final String CHAT_MSG_TYPE = "chat_msg_type";
@@ -32,13 +28,9 @@ public class ChatTable {
             + TABLE_NAME + " ("
             + "[_id] integer autoinc primary key,"
             + USER_ID + " integer,"
-            + USER_NAME + " varchar(32),"
-            + USER_HEAD + " varchar(32),"
-            + TARGET_ID + " integer,"
-            + TARGET_NAME + " varchar(32),"
-            + TARGET_HEAD + " varchar(32),"
-            + TARGET_EXPAND_RELATION + " varchar(16),"
-            + CHAT_MSG_CONTENT + " varchar(1024),"
+            + FRIEND_ID + " integer,"
+            + FRIEND_EXPAND_RELATION + " varchar(16),"
+            + CHAT_MSG_CONTENT + " text,"
             + CHAT_MSG_TIME + " datetime,"
             + CHAT_MSG_TYPE + " varchar(10))";
 
