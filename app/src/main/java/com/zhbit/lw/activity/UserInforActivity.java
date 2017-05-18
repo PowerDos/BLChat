062ba4966382dee885c69302e933ded13cc50aeb
wjh876531738 <876531738@qq.com>
2017-05-18 19:28:17 +0800

    添加用户信息界面

---
+++ app/src/main/java/com/zhbit/lw/activity/UserInforActivity.java
@@ -0,0 +1,55 @@
+package com.zhbit.lw.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.zhbit.lw.blchat.R;
+import com.zhbit.lw.model.Model;
+import com.zhbit.lw.model.bean.UserInfo;
+import com.zhbit.lw.model.dao.UserTable;
+
+public class UserInforActivity extends AppCompatActivity {
+
+    private int userId;
+    private UserInfo userInfor;
+
+    private TextView userName, userAccount, userLocation, userSex, userSign;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_inofr);
+
+        initView();
+        initData();
+        initEvent();
+
+    }
+
+    public void initView() {
+        userName = (TextView) findViewById(R.id.userInfor_userName);
+        userAccount = (TextView) findViewById(R.id.userInfor_userAccount);
+        userLocation = (TextView) findViewById(R.id.userInfor_userLocation);
+        userSex = (TextView) findViewById(R.id.userInfor_userSex);
+        userSign = (TextView) findViewById(R.id.userInfor_userSign);
+    }
+
+    public void initData() {
+
+        userId = getIntent().getIntExtra(UserTable.USER_ID, -1);
+        userInfor = Model.getInstance().getDbManager().getUserTableDao().getUserInforByAccount(userId);
+
+        userName.setText(userInfor.getUserName());
+        userAccount.setText(userInfor.getUserAccount());
+        userLocation.setText(userInfor.getUserLocation());
+        userSex.setText(userInfor.getUserSex());
+        userSign.setText(userInfor.getUserSign());
+
+    }
+
+
+    public void initEvent() {
+    }
+
+}
--- app/src/main/java/com/zhbit/lw/fragment/MeFragment.java
+++ app/src/main/java/com/zhbit/lw/fragment/MeFragment.java
@@ -1,5 +1,7 @@
 package com.zhbit.lw.fragment;
 
+import android.content.Intent;
+import android.media.MediaRouter;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -7,12 +9,15 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.zhbit.lw.activity.UserInforActivity;
 import com.zhbit.lw.blchat.R;
 import com.zhbit.lw.model.bean.UserInfo;
 import com.zhbit.lw.model.Model;
+import com.zhbit.lw.model.dao.UserTable;
 
 /**
  * Created by wjh on 17-5-6.
@@ -23,6 +28,9 @@ public class MeFragment extends Fragment{
     private View view;
     private ImageView ivUserHead;
     private TextView tvUserName, tvUserAccount;
+    private LinearLayout layoutUserInfor;
+
+    private int userId;
 
     @Nullable
     @Override
@@ -45,17 +53,29 @@ public class MeFragment extends Fragment{
         ivUserHead = (ImageView) view.findViewById(R.id.userInfor_userHead);
         tvUserName = (TextView) view.findViewById(R.id.userInfor_userName);
         tvUserAccount = (TextView) view.findViewById(R.id.userInfor_userAccount);
+
+        layoutUserInfor = (LinearLayout) view.findViewById(R.id.lineLayoutEditMyInfo);
     }
 
     private void initData() {
         // 从数据库获取数据
-        UserInfo userInfo = Model.getInstance().getDbManager().getUserTableDao().getUserInforByAccount();
+        userId = 1;
+        UserInfo userInfo = Model.getInstance().getDbManager().getUserTableDao().getUserInforByAccount(userId);
 
         tvUserName.setText(userInfo.getUserName());
+        tvUserAccount.setText("帐号：" + userInfo.getUserAccount());
 
     }
 
     private void initEvent() {
+        layoutUserInfor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), UserInforActivity.class);
+                intent.putExtra(UserTable.USER_ID, 1);
+                startActivity(intent);
+            }
+        });
     }
 
 }
